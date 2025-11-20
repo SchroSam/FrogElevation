b862ad19e68356c40f2515044737f253d448bff3
Schrod <schrosam@gmail.com>
2025-11-20 16:08:05 -0600

    Basic vertical charge jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
     public float jumpChargeMax = 2f;
     public float jumpCharge = 0f;
     public float chargeMultiplier = 1f;
+    public float sinPeriod = 0.006f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,9 +16,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
             jumpCharge += Time.deltaTime * chargeMultiplier;
+
+            //GetComponent<SpriteRenderer>().color = new Color(Mathf.Sin(400 * jumpCharge * sinPeriod) * 125 + 125, Mathf.Sin(400 * jumpCharge * sinPeriod) * 125 + 125, Mathf.Sin(400 * jumpCharge * sinPeriod) * 125 + 125);
 
             if(jumpCharge > jumpChargeMax)
                 jumpCharge = jumpChargeMax;
@@ -26,6 +29,7 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKeyUp(KeyCode.Space))
         {
             GetComponent<Rigidbody2D>().AddForceY(jumpCharge);
+            GetComponent<SpriteRenderer>().color = Color.white;
             jumpCharge = 0;
         }
     }
