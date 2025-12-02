5164ad086331d0d1f032821fdfe18b90433e986d
Schrod <schrosam@gmail.com>
2025-12-02 14:24:46 -0600

    Added jump aiming

---
+++ Assets/Scripts/Cam.cs
@@ -0,0 +1,50 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Cam : MonoBehaviour
+{
+    public GameObject target;
+    public int type;
+    public float smoothTime = 0.3f;    // Damping time
+    private Vector3 velocity = Vector3.zero;
+    Vector3 targetTransform;
+    public float yOffset = 1;
+
+    private bool camLock = false;
+
+    public float maxHeight = 99999;
+
+    // Update is called once per frame
+    private void Start()
+    {
+
+
+    }
+    void Update()
+    {
+        //if (Math.Abs(target.GetComponent<Rigidbody2D>().linearVelocity.y) > minYspeed)
+        //{
+        targetTransform = target.transform.position;
+        targetTransform.z = transform.position.z;
+        targetTransform.y = target.transform.position.y + yOffset;
+
+        //if(transform.position.y + (GetComponent<Camera>().orthographicSize / 2) < maxHeight)
+            transform.position = Vector3.SmoothDamp(transform.position, targetTransform, ref velocity, smoothTime);
+
+        if(transform.position.y + (GetComponent<Camera>().orthographicSize / 2) > maxHeight || camLock)
+        {
+            camLock = true;
+            //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(transform.position.x, transform.position.y - 3, transform.position.z), ref velocity, smoothTime);
+            transform.position = new Vector3(transform.position.x, 1.07f, transform.position.z);
+            transform.GetComponent<Camera>().orthographicSize = 6.93f;
+        }
+
+
+    }
+        // else
+        // {
+        //     transform.position = new Vector3(target.transform.position.x, yspot, -10);
+        // }
+        // }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,34 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float jumpChargeMax = 2f;
     public float jumpCharge = 0f;
     public float chargeMultiplier = 1f;
+    public float dirAbsMax = 1f;
+    public float dirVal = 0f;
+    public float dirMult = 1f;
     public float sinPeriod = 0.006f;
+    private GameObject chargeMeter;
+    private GameObject dirMeter;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        chargeMeter = GameObject.Find("ChargeMeter");
+        dirMeter = GameObject.Find("DirMeter");
+
+        dirMeter.GetComponent<Slider>().maxValue = dirAbsMax;
+        dirMeter.GetComponent<Slider>().minValue = -dirAbsMax;
+        dirMeter.GetComponent<Slider>().value = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Jump Logic
         if (Input.GetKey(KeyCode.Space))
         {
             jumpCharge += Time.deltaTime * chargeMultiplier;
@@ -29,8 +42,38 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKeyUp(KeyCode.Space))
         {
             GetComponent<Rigidbody2D>().AddForceY(jumpCharge);
+            GetComponent<Rigidbody2D>().AddForceX(dirVal);
             GetComponent<SpriteRenderer>().color = Color.white;
+            dirVal = 0;
             jumpCharge = 0;
         }
+
+        chargeMeter.GetComponent<Slider>().value = jumpCharge / jumpChargeMax;
+
+
+        //Aiming Logic
+        if (Input.GetKey(KeyCode.A))
+        {
+            dirVal -= Time.deltaTime * dirMult;
+
+            if(Math.Abs(dirVal) > dirAbsMax)
+                dirVal = -dirAbsMax;
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            dirVal += Time.deltaTime * dirMult;
+
+            if(Math.Abs(dirVal) > dirAbsMax)
+                dirVal = dirAbsMax;
+        }
+
+
+        dirMeter.GetComponent<Slider>().value = dirVal;
+
+
+        //Tongue Grab logic
+        //TODO
+
     }
 }
